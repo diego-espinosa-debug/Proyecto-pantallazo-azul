6ac501f4caa31a2975cc3556c34691ebd8147173
dariksonli <142368144+dariksonli@users.noreply.github.com>
2023-11-01 23:01:33 +0000

    Probando los gitxxx

--- .ccls-cache/@home@runner@Proyectop/main.c
+++ .ccls-cache/@home@runner@Proyectop/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-
+//yaaaaa ahora 
 //probando los git para el proyectoxxxxxxxx
 int main(void) {
   printf("Hola proyectoxx\n");
-  
+  printf("Que pasa realmente con los gitxxxxxxx\n");
   return 0;
 }
