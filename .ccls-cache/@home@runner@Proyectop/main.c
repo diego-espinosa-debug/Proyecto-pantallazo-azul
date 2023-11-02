79692157c51f77a98461477606ccc25c1f665a0a
dariksonli <142368144+dariksonli@users.noreply.github.com>
2023-11-02 02:22:41 +0000

    nose

--- .ccls-cache/@home@runner@Proyectop/main.c
+++ .ccls-cache/@home@runner@Proyectop/main.c
@@ -3,7 +3,7 @@
 //yaaaaa ahora 
 //probando los git para el proyectoxxxxxxxx
 int main(void) {
-  printf("Hola proyectoxx\n");
+  //printf("Hola proyectoxx\n");
   printf("Que pasa realmente con los gitxxxxxxx\n");
   return 0;
 }
