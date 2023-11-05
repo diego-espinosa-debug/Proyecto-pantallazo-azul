48bb94bf056572610d69b07c3be2b35b59d3d415
javo1021 <127540791+javo1021@users.noreply.github.com>
2023-11-05 21:49:32 +0000

    Crear funcion validarInstruccion

--- .ccls-cache/@home@runner@Proyectop/main.c
+++ .ccls-cache/@home@runner@Proyectop/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>>
 typedef struct animal{
   char nombre[100];
   int numero;
@@ -22,15 +22,27 @@ typedef struct vegetal{
 //Exploración y Búsqueda. Se implementaran formas para que puedas buscar de manera específica el animal o vegetal que desee el usuario
 //Educación sobre especies. Se informará de manera breve sobre el vegetal y como el usuario podría contribuir para mantener a salvo esta especie
 }vegetal;
+int validarInstruccion();
+void mostrarOpciones();
+
+
 
 int main(){
   printf("**BioDex**\n\n");
   printf("Bienvenido al BioDex, el lugar donde podrás encontrar información sobre animales y vegetales en camino a la extinción.\n\n");
+
+  mostrarOpciones(); // se mostraran todas las opciones disponibles en el menu principal 
+  int instruccion;
+  instruccion = validarInstruccion();
+
+
+
+
   
   //--------------------------------------------------------
   //hacer una funcion que importe un archivo con animales y vegetales, ingresarlos en memoria.
   //--------------------------------------------------------
-
+  //importarInfoBiodex();
   
   //luego ingresarlo en los distintos mapas creados a continuacion
   
@@ -41,10 +53,42 @@ int main(){
   //animales
   //herbivoro, carnivoro o omnivoro
   //terrestre, acuatico o volador
+  //mamifero, ave, reptil, anfibio, peces
 
   //vegetales
   //arbol o hongo
   //acuatico o continental
-  
+
+  //---------------------------------------------------
+  //funcion para ingresar animales y vegetales en mapas
+  //---------------------------------------------------
   return EXIT_SUCCESS;
+}
+
+int validarInstruccion(){
+  char aux[10];
+  
+  while(1){
+    printf("Que desea hacer: \n");
+
+    if(scanf("%9s", aux) != 1){
+      printf("debe ingresar una opcion valida\n");
+      while(getchar() != '\n');
+    } else {
+        int instruccion;
+        if(strlen(aux) == 1 &&  aux[0] >= '0' && aux[0] <= '9')/* hay que ver el maximo de instrucciones y remplazarlo en el 9 */{
+        instruccion = atoi(aux);
+        while(getchar() != '\n');
+        return instruccion;
+      } else{
+          printf("debe ingresar una opcion valida\n");
+          mostrarOpciones();
+          while(getchar() != '\n');
+      }
+    }
+  }  
+}
+
+void mostrarOpciones(){
+  return;
 }
---
+++ info.md
@@ -0,0 +1,7 @@
+Según datos de la Unión Internacional para la Conservación de la Naturaleza (UICN), el organismo internacional con mayor potestad sobre el problema, aproximadamente 5.200 especies de animales se encuentran en peligro de extinción en la actualidad. Además, en un desglose por clase, se encuentran en peligro de extinción el 11% de las aves, el 20% de los reptiles, el 34% de los peces y 25% de los anfibios y mamíferos.
+
+entre las mayores amenazas se encuentra la destrucción y fragmentación de sus hábitats; el cambio climático; la caza y tráfico ilegal; y la introducción de especies exóticas. 
+
+¿Cómo evitar la extinción de especies?
+implica la puesta en marcha de una gran cantidad de recursos y acciones concretas. Algunas de ellas serían evitar la fragmentación de sus hábitats, por ejemplo la deforestación; perseguir y castigar con dureza la caza ilegal y el tráfico de especies; la creación de reservas naturales; o el fomento de programas de reproducción, reintroducción y de mejora genética. La lucha contra la contaminación y el cambio climático también es la lucha por la conservación de muchos animales. Cada decisión personal tiene una consecuencia a nivel global en múltiples aspectos, de ahí la importancia de la concienciación social de la población para luchar contra la pérdida de biodiversidad. 
+
